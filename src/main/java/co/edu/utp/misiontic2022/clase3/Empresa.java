a097e524b2ee8dfd1db240b8e71dc78ef0bcf2c5
fabian-bejarano <fabian.bejarano21@gmail.com>
2021-07-20 15:17:31 -0500

    clase 03

--- src/main/java/co/edu/utp/misiontic2022/clase3/Empresa.java
+++ src/main/java/co/edu/utp/misiontic2022/clase3/Empresa.java
@@ -1,5 +1,18 @@
 package co.edu.utp.misiontic2022.clase3;
 
 public class Empresa {
+
+    private String nombre;
+    private Empleado empleado;
+
+    public Empresa(String nombre, Empleado empleado){
+        this.nombre = nombre;
+        this.empleado = empleado;
+
+    }
     
+    public String toString(){
+        return"Empresa : " + nombre + "Empleado: " + empleado;
+        
+    }
 }
