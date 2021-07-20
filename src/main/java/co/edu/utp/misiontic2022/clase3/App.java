c9c69aa642ec47df7d43e69fc9b165f513186a6e
fabian-bejarano <fabian.bejarano21@gmail.com>
2021-07-20 15:30:19 -0500

    clase 03

--- src/main/java/co/edu/utp/misiontic2022/clase3/App.java
+++ src/main/java/co/edu/utp/misiontic2022/clase3/App.java
@@ -13,6 +13,7 @@ public class App
     {
         Persona objPersona1 = new Empleado("Byron", 20, 10546.32);
         Persona objPersona2 = new Cliente("JAvier", 30, "(032) 446 23 45");
+        // polimorfismo: capacidad que tiene un metodo de comportarse de diferentes maneras segun el onjeto que lo llame . ej mostrar
 
         objPersona1.mostrar();
         objPersona2.mostrar();
@@ -20,6 +21,11 @@ public class App
         System.out.println(objPersona1);
         System.out.println(objPersona2);
 
-        
+        // se crea el objeto de empresa para validar la relacion entre empresa y empleado
+        //composicion ---> relacion fuerte  --> se hace atraves del atributo de la clase.
+
+        Empresa objEmpresa = new Empresa("MisionTIC2022",(Empleado) objPersona1);
+        System.out.println(objEmpresa);
     }
+    
 }
